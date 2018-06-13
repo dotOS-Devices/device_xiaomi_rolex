b714fbd4c83d74ab184aa26316b45e1215ced18d
AryanPatidar <ap9977925180@gmail.com>
2018-06-13 14:27:01 +0000

    Fixed Error

--- product/vibrator.mk
+++ product/vibrator.mk
@@ -1,8 +1,4 @@
 # Vibrator HAL
 PRODUCT_PACKAGES += \
-<<<<<<< HEAD
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
-=======
-    android.hardware.vibrator@1.0-service.lineage
->>>>>>> parent of 30bc402... rolex -> riva
