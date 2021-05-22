26f5c05d2ad3fdba6d52477da70d7209feb4e77a
Mohan C M <mohanmanjappa@gmail.com>
2021-05-22 16:09:33 +0000

    rolex: Use msm8996-los hals
    Signed-off-by: adi153 <singhmanohar6699@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,6 +187,9 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
+#Override hals
+OVERRIDE_QCOM_HARDWARE_VARIANT := msm8996-los
+
 # Init
 #TARGET_INIT_VENDOR_LIB         := libinit_rolex
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_rolex
